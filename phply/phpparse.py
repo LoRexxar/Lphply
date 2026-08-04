98c608370ddcdfb991a3e3bc357f8251839abaeb
LoRexxar <LoRexxar@gmail.com>
2026-08-04 11:07:29 +0800

    feat(parse): allow reserved words (list/class/const) as method names in static calls

--- phply/phpparse.py
+++ phply/phpparse.py
@@ -1288,8 +1288,16 @@ def p_function_call_static(p):
     '''function_call : class_name DOUBLE_COLON STRING LPAREN function_call_parameter_list RPAREN
                      | class_name DOUBLE_COLON variable_without_objects LPAREN function_call_parameter_list RPAREN
                      | variable_class_name DOUBLE_COLON STRING LPAREN function_call_parameter_list RPAREN
-                     | variable_class_name DOUBLE_COLON variable_without_objects LPAREN function_call_parameter_list RPAREN'''
+                     | variable_class_name DOUBLE_COLON variable_without_objects LPAREN function_call_parameter_list RPAREN
+                     | class_name DOUBLE_COLON reserved_method_name LPAREN function_call_parameter_list RPAREN
+                     | variable_class_name DOUBLE_COLON reserved_method_name LPAREN function_call_parameter_list RPAREN'''
     p[0] = ast.StaticMethodCall(p[1], p[3], p[5], lineno=p.lineno(2))
+
+def p_reserved_method_name(p):
+    '''reserved_method_name : LIST
+                            | CLASS
+                            | CONST'''
+    p[0] = p[1]
 
 def p_function_call_static_dynamic_name(p):
     '''function_call : class_name DOUBLE_COLON LBRACE expr RBRACE LPAREN function_call_parameter_list RPAREN
