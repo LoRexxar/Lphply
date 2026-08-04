d0cbe0835d3fa8dfc1f6e940ff23828b4e7a53c7
LoRexxar <LoRexxar@gmail.com>
2026-08-04 11:07:29 +0800

    feat(parse): callable invocation (expr)(args)

--- phply/phpparse.py
+++ phply/phpparse.py
@@ -1297,8 +1297,12 @@ def p_function_call_static_dynamic_name(p):
     p[0] = ast.StaticMethodCall(p[1], p[4], p[7], lineno=p.lineno(2))
 
 def p_function_call_variable(p):
-    'function_call : variable_without_objects LPAREN function_call_parameter_list RPAREN'
-    p[0] = ast.FunctionCall(p[1], p[3], lineno=p.lineno(2))
+    '''function_call : variable_without_objects LPAREN function_call_parameter_list RPAREN
+                     | LPAREN expr RPAREN LPAREN function_call_parameter_list RPAREN'''
+    if len(p) == 5:
+        p[0] = ast.FunctionCall(p[1], p[3], lineno=p.lineno(2))
+    else:
+        p[0] = ast.FunctionCall(p[2], p[5], lineno=p.lineno(4))
 
 def p_function_call_backtick_shell_exec(p):
     'function_call : BACKTICK encaps_list BACKTICK'
